d9a3223ea2909f3bc6559b01c8f2cec935150936
Byron Guina <26263630+ByronGuina@users.noreply.github.com>
2022-03-14 16:40:51 -0600

    Add caching to front page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,13 @@
-import { LoaderFunction, useLoaderData } from 'remix'
+import { json, LoaderFunction, useLoaderData } from 'remix'
 import { fromNotionBlock } from '~/components/notion/block'
 import { getFrontPage } from '~/db.server'
 
 export const loader: LoaderFunction = async () => {
-    return await getFrontPage()
+    return json(await getFrontPage(), {
+        headers: {
+            'Cache-Control': 'max-age=3600, stale-while-revalidate=60',
+        },
+    })
 }
 
 export default function Index() {
