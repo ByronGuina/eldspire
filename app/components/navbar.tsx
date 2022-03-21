2ee8ea61ccaa431e08a77b9bf614ad243a18fb6a
Byron Guina <26263630+ByronGuina@users.noreply.github.com>
2022-03-21 17:22:46 -0600

    Add search box animations

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import { useEffect, useRef, useState, Ref, forwardRef } from 'react'
 import { Link } from 'remix'
 
@@ -49,7 +50,7 @@ export function Navbar() {
                 <button onClick={toggle} className="focus" placeholder="Search for pages">
                     Search
                 </button>
-                {isOpen && <Search ref={ref} />}
+                <AnimatePresence exitBeforeEnter>{isOpen && <Search ref={ref} />}</AnimatePresence>
             </div>
         </nav>
     )
@@ -58,8 +59,23 @@ export function Navbar() {
 function SearchModal({}, ref: Ref<HTMLInputElement>) {
     return (
         <div className="absolute left-0 top-0 w-screen h-screen flex">
-            <div className="absolute left-0 top-0 w-full h-full bg-text opacity-40 z-10" />
-            <input ref={ref} className="m-auto border border-text w-1/3 px-4 py-3 bg-background z-30 mt-24 focus" />
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 0.4 }}
+                exit={{ opacity: 0 }}
+                transition={{ type: 'spring', mass: 1, damping: 20, stiffness: 200 }}
+                className="absolute left-0 top-0 w-full h-full bg-text z-10"
+            />
+            <motion.input
+                initial={{ opacity: 0, y: -10, scale: 0.95 }}
+                animate={{ opacity: 1, y: 10, scale: 1 }}
+                exit={{ opacity: 0, y: -10, scale: 1.05 }}
+                transition={{ type: 'spring', mass: 1, damping: 20, stiffness: 200 }}
+                ref={ref}
+                type="search"
+                placeholder="Eldspire, Baalam Pixal"
+                className="m-auto border border-text w-1/3 px-4 py-3 bg-background z-30 mt-24 focus placeholder-text placeholder-opacity-40"
+            />
         </div>
     )
 }
