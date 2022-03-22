8e26e9f536339c11152cdcd431ab0df884032aab
Byron Guina <26263630+ByronGuina@users.noreply.github.com>
2022-03-21 22:03:21 -0600

    Remind myself to extract block rendering code

--- app/components/notion/block.tsx
+++ app/components/notion/block.tsx
@@ -1,6 +1,8 @@
 import { textToComponents } from './text-to-components'
 import { NotionBlock } from './types'
 
+// TODO: This could be passed in as a prop if I want to use
+// other components than these hardcoded ones
 const toHtmlTag = {
     heading_1: 'h1',
     heading_2: 'h2',
@@ -12,6 +14,9 @@ const toHtmlTag = {
 export function Block({ block }: { block: NotionBlock }) {
     const blockType = block.type as keyof typeof toHtmlTag
     const Tag = toHtmlTag[blockType] as keyof JSX.IntrinsicElements
+
+    // TODO: Make block and text-to-components a bit simplier using
+    // recursion or some shit
     const children = textToComponents(block)
 
     if (block.type === 'image') {
