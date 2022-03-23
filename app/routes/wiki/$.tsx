9440cb44e55c6b64ed29c1f30af85fb732e76fcf
Byron Guina <26263630+ByronGuina@users.noreply.github.com>
2022-03-22 22:29:35 -0600

    Update fallback copy

--- app/routes/wiki/$.tsx
+++ app/routes/wiki/$.tsx
@@ -4,7 +4,7 @@ export default function Fallback() {
             <h1>That page doesn't exist.</h1>
             <p>
                 Uh oh... looks like my traffic coordination bots sent you to the wrong place. They will be reprimanded.
-                Make sure the page you were trying to visit has the correct slug.
+                If this seems like an error, make sure the page you were trying to visit has the correct slug.
             </p>
         </div>
     )
