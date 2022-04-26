3154ef759fa6178c8647d9790c9ac54d4a68f0cd
Byron Guina <26263630+ByronGuina@users.noreply.github.com>
2022-04-26 10:38:11 -0600

    Update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     darkMode: 'class',
-    purge: {
-        content: ['./app/**/*.{js,jsx,ts,tsx,md,mdx}', './components/**/*.{js,jsx,ts,tsx,md,mdx}'],
-    },
+    content: ['./app/**/*.{js,jsx,ts,tsx,md,mdx}'],
     theme: {
         fontFamily: {
             serif: ['"Iowan Old Style"', 'Iowan', 'Newsreader', 'serif'],
